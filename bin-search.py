6381fb7f7b2efd09cb0ecc4dea7a652b96ebd864
a-lemus96 <alejandro.lemus@cimat.mx>
2023-03-21 15:30:51 -0600

    feat: Add non-recursive binary search fn
    Non-recursive binary search receives an array A as input and a key value
    to search for. It returns key index when search is succesful and returns
    None when item was not found.

---
+++ bin-search.py
@@ -0,0 +1,30 @@
+# standard library modules
+import argparse
+from typing import List
+
+def binary_search(A: List, key: int) -> int:
+    """Non recursive binary search function. Uses a while loop instead of
+    recursive function calls.
+    ---------------------------------------------------------------------------- 
+    Args:
+        A: list of sorted integers
+        key: element to be searched for within the list
+    Returns:
+        idx: list index where the first occurence was found. Returns None if key
+             is not present"""
+    if len(A) == 0:
+        return None
+    
+    # initialize idxs for slicing A
+    p = 0
+    q = len(A)
+
+    while len(A[p: q])> 0:
+        median = (p + q - 1) // 2
+        if A[median] == key:
+            return median
+        else:
+            if key < A[median]:
+                q = median
+            else:
+                p = median + 1
